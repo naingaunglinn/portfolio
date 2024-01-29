78b4262d61e1e39f585507f5ba3988dbb503e0de
naingaunglinn <naingaunglinn369@gmail.com>
2024-01-29 15:27:55 +0630

    update: nav item gap

--- app/header.tsx
+++ app/header.tsx
@@ -10,7 +10,7 @@ const Header = ({nav}:navProps) => {
     return(
         <header className="z-10 fixed top-8 w-full">
             <div className="flex justify-center text-[18px] font-ti ">
-                <div className="shadow-sm backdrop-blur-sm dark:bg-white/10 bg-black/30 flex justify-center rounded-full p-2">
+                <div className="shadow-sm backdrop-blur-sm dark:bg-white/10 bg-black/30 flex justify-center gap-4 rounded-full p-2">
                     {nav.map((content,index) => {
                         return(
                             <div key={index} className="px-4 dark:text-white text-black dark:hover:bg-black hover:text-black hover:bg-white dark:hover:text-white hover:rounded-full font-bold "><Link href={content.slug} className="">{content.name}</Link></div>
