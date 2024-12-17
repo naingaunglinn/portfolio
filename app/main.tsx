f7745ba751815011f59e5a13079346cf056980a2
Naing Aung Linn <51980574+naingaunglinn@users.noreply.github.com>
2024-12-17 07:40:42 +0630

    update: flex wrap

--- app/main.tsx
+++ app/main.tsx
@@ -9,16 +9,16 @@ import TextBounce from "./components/TextBounce"
 const Main = () => {
     return(
       	<div>          
-			<div className="flex">
-				<Heading text="Naing Aung Linn" />
-				<HeadTag text="404" />
-			</div>			  
-			<br/>
-			<TextBounce />
-			<p className="text-justify text-[18px] mt-2">An optimistic, flexible, and team-oriented web developer, dedicated to inspiring innovation and fostering collaboration. Committed to excellence in every project, I strive to drive positive change through coding. My journey is defined by a dedication to continuous growth and adaptation, as I seek to push the boundaries of what is possible in the world of coding.</p>
-			<div className="mt-20">
-				<Link href={'/contact'} className="text-[30px] bg-black text-white dark:bg-white dark:text-black transition-colors duration-300 ease-in-out hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white hover:shadow-sm p-2">Contact Me!</Link>
-			</div>
+		<div className="flex flex-wrap">
+			<Heading text="Naing Aung Linn" />
+			<HeadTag text="404" />
+		</div>			  
+		<br/>
+		<TextBounce />
+		<p className="text-justify text-[18px] mt-2">An optimistic, flexible, and team-oriented web developer, dedicated to inspiring innovation and fostering collaboration. Committed to excellence in every project, I strive to drive positive change through coding. My journey is defined by a dedication to continuous growth and adaptation, as I seek to push the boundaries of what is possible in the world of coding.</p>
+		<div className="mt-20">
+			<Link href={'/contact'} className="text-[30px] bg-black text-white dark:bg-white dark:text-black transition-colors duration-300 ease-in-out hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white hover:shadow-sm p-2">Contact Me!</Link>
+		</div>
         </div>
     )
 }
