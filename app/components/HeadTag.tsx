7ddfb15e398237fa79392e23dc7c2b1ebc6f4029
Naing Aung Linn <51980574+naingaunglinn@users.noreply.github.com>
2024-12-17 07:43:05 +0630

    update: extra spaces

--- app/components/HeadTag.tsx
+++ app/components/HeadTag.tsx
@@ -14,7 +14,7 @@ const HeadTag = ({text}:props) => {
             transition={{ duration: 1.5, type: 'spring', bounce: 0.5, }}
             
         >
-            <span className="bg-black text-white dark:bg-white dark:text-black inline text-[70px] font-bold my-2 ml-2 px-2 hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white duration-300">{text}</span>
+            <span className="bg-black text-white dark:bg-white dark:text-black inline text-[70px] font-bold my-2 px-2 hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white duration-300">{text}</span>
         </motion.div>
     )
 }
