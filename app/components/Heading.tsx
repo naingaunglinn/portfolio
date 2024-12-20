da65ab3d244baca23c6ba739543d402a03fa1852
Naing Aung Linn <51980574+naingaunglinn@users.noreply.github.com>
2024-12-20 14:42:50 +0630

    removed: element opacities

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -10,8 +10,8 @@ type props = {
 const Heading = ({text}:props) => {
     return (
         <motion.div
-            initial={{ y: -200, opacity: 0}}
-            animate={{ y: 0, opacity: 1}}
+            initial={{ y: -200}}
+            animate={{ y: 0}}
             transition={{ duration: 1.5, type: 'spring', bounce: 0.5, }}
         >
             <h1 className="bg-black text-white dark:bg-white dark:text-black inline text-[70px] font-bold my-2 px-2 hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white duration-300">
