d45136ce26c2063d84068166b33eaba87b346783
naingaunglinn <naingaunglinn369@gmail.com>
2024-12-21 21:41:25 +0630

    add: project info again

--- app/page.tsx
+++ app/page.tsx
@@ -58,6 +58,26 @@ export default function Home() {
                     />
                 </div>
             </div>
+            <div className="grid grid-cols-5 grid-flow-col gap-4 justify-between pt-4">
+                <div className="col-span-1">
+                    <Image
+                        className="h-auto max-w-full rounded-lg"
+                        src="/hobby/img-3.JPG"
+                        alt="buffelo"
+                        width={1000}
+                        height={1000}
+                    />
+                </div>
+                <div className="col-span-4">
+                    <span className="bg-black text-white dark:bg-white dark:text-black px-2 text-[20px] font-bold hover:-translate-y-1 hover:scale-110 hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white duration-300">Key Persons Event Management Application</span>
+                    <p className="text-justify text-[18px] mt-2   ">
+                    Developed a comprehensive event management application that allows users to create, edit, and join events online. Utilized React for the front end to provide a dynamic and interactive user experience. Employed TypeScript for type safety and enhanced code quality. Implemented GraphQL for efficient data fetching and manipulation. Used MUI for building consistent and visually appealing UI components. Managed version control using Git to ensure smooth collaboration and code management. Leveraged AWS Lambda for serverless back-end functions, ensuring scalability and cost-efficiency. Integrated a chat feature that allows participants and organizers to communicate seamlessly within the application.
+                    </p>
+                    <div className="mt-8">
+                        <Link href={'/project/1'} className="text-[30px] bg-black text-white dark:bg-white dark:text-black transition-colors duration-300 ease-in-out hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white hover:shadow-sm p-2">Seek more &gt;&gt;&gt;</Link>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     </main>
